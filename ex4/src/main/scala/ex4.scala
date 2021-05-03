12296184a6bbdb324e1790745ed959a112a7d18e
JinMinghan18 <1099461301@qq.com>
2021-05-03 18:16:57 +0800

    实验4
    rdd

---
+++ ex4/src/main/scala/ex4.scala
@@ -0,0 +1,19 @@
+import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
+
+object ex4 {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+    conf.setMaster("local[*]").setAppName("ex4")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("WARN")
+    val data = sc.textFile("input1",3)
+//    val res = data.filter(_.trim().length>0).map(row => (row.split( " ")(0),
+//    row.split(" ")(1))).partitionBy(new HashPartitioner(1)).distinct()
+    val avg = data.map(line=> (line.split(" ")(0),line.split(" ")(1).toDouble
+))
+  .partitionBy(new HashPartitioner(1)).mapValues(x=>(x,1))
+  .reduceByKey((x,y)=>(x._1+y._1,x._2+y._2)).mapValues(x=>(x._1/x._2))
+    avg.saveAsTextFile("output1")
+    avg.collect().foreach(println)
+  }
+}
